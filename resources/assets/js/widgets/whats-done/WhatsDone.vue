62f7a6f5ba28998197b54b5d4c77dd5cc0702ea2
Boris Strahija <bstrahija@gmail.com>
2016-11-09 11:57:28 +0100

    Add tečaj

--- resources/assets/js/widgets/whats-done/WhatsDone.vue
+++ resources/assets/js/widgets/whats-done/WhatsDone.vue
@@ -3,7 +3,7 @@
         <div class="backdrop">WHATSDONE</div>
 
         <div class="panel-body">
-
+            <iframe frameborder="0" scrolling="no" height="131" width="453" allowtransparency="true" marginwidth="0" marginheight="0" src="http://fxrates.investing.com/index.php?force_lang=1&pairs_ids=1643;1757;1;2;&header-text-color=%23FFFFFF&curr-name-color=%230059b0&inner-text-color=%23000000&green-text-color=%232A8215&green-background=%23B7F4C2&red-text-color=%23DC0001&red-background=%23FFE2E2&inner-border-color=%23CBCBCB&border-color=%23cbcbcb&bg1=%23F6F6F6&bg2=%23ffffff&bid=show&ask=show&last=hide&change=hide&last_update=hide"></iframe>
         </div>
     </div>
 </template>
