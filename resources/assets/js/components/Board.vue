d9eb1d4cfc25d6d2e0a66ba97cf489e9fd440ff9
Boris Strahija <bstrahija@gmail.com>
2016-11-11 14:11:51 +0100

    Auto refresh board

--- resources/assets/js/components/Board.vue
+++ resources/assets/js/components/Board.vue
@@ -4,7 +4,24 @@
 import Packery from 'packery';
 
 export default {
+    data () {
+        return {
+            updateInterval            : false,
+            updateIntervalLength      : 30000,
+        }
+    },
+
     mounted() {
+        this.updateInterval = setInterval(() => {
+            $.getJSON('/api/status', (status) => {
+                if (status.should_refresh) {
+                    document.location = document.location;
+                }
+            }).fail(() => {
+                // Ha?
+            });
+        }, this.updateIntervalLength);
+
         let pckry = new Packery('.board-grid .wrapper', {
             itemSelector: '.grid-item',
             gutter: 0
