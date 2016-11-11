3833096cf8cee9e1b5a625a53c09d37ae5e54df1
Boris Strahija <bstrahija@gmail.com>
2016-11-11 14:11:58 +0100

    Rotate logos

--- resources/assets/js/widgets/whats-done/WhatsDoneSlideshow.vue
+++ resources/assets/js/widgets/whats-done/WhatsDoneSlideshow.vue
@@ -18,27 +18,8 @@ export default {
 
                 // Get random user
                 let nextUser = users[Math.floor(Math.random() * users.length)];
-                console.log(nextUser.id);
 
                 this.showUser = nextUser.id;
-
-                // this.carousel = new Slick($('.slideshow')[9], {
-                //     infinite: true,
-                //     slidesToShow: 1,
-                //     slidesToScroll: 1
-                // });
-
-                // if ( ! this.flkty) {
-                //     this.flkty = new Flickity(document.querySelector('.slideshow'), {
-                //         autoPlay: 2000,
-                //         // prevNextButtons: false,
-                //         pageDots: false,
-                //     });
-                // } else {
-                //     this.flkty.prepend( $('<div class="slide"><div class="cont"><h5>Boris Strahija</h5> <div class="tasks"><span><span>OptimizeLeads - CDN fix, dev test and setup, </span></span></div></div></div>') );
-                //     this.flkty.reloadCells();
-                //     console.log(this.flkty.cells.length);
-                // }
             }).fail(() => {
                 this.users = [];
             });
