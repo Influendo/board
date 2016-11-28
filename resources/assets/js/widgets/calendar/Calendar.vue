db19f8b2add785bf50e9559117afb5ea8ba73bfa
Franjo Filo <fffilo666@gmail.com>
2016-11-28 15:40:23 +0100

    Widget calendar

--- resources/assets/js/widgets/calendar/Calendar.vue
+++ resources/assets/js/widgets/calendar/Calendar.vue
@@ -12,7 +12,10 @@ export default {
         return $.extend({}, this.$options._defaults, {
             url: null,
             interval: null,
-            delay: 500
+            delay: 500,
+            lastDate: "0000-00-00",
+            monthNames: [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ],
+            weekdayNames: [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ]
         });
     },
 
@@ -23,8 +26,9 @@ export default {
 			var response = {}
 			response.y = date.getYear();
 			response.Y = date.getFullYear();
-			response.m = date.getMonth();
+			response.m = date.getMonth() + 1;
 			response.M = ("0" + response.m).slice(-2);
+			response.w = date.getDay();
 			response.d = date.getDate();
 			response.D = ("0" + response.d).slice(-2);
 			response.h = date.getHours();
@@ -33,6 +37,36 @@ export default {
 			response.I = ("0" + response.i).slice(-2);
 			response.s = date.getSeconds();
 			response.S = ("0" + response.s).slice(-2);
+
+			response.MMMM = this.monthNames[response.m - 1];
+			response.MMM  = response.MMMM.substr(0, 3);
+			response.WWWW = this.weekdayNames[response.w || this.weekdayNames.length - 1];
+			response.WWW  = response.WWWW.substr(0, 3);
+
+			response.lastDate = response.Y + "-" + response.M + "-" + response.D;
+			response.cal      = this.response ? this.response.cal : null;
+			if (!this.response || response.lastDate != this.response.lastDate) {
+				console.log("render calendar")
+				var firstDayOfWeek = new Date(response.Y, response.m - 1, 1).getDay();
+				var monthCountLast = new Date(response.Y, response.m - 1, 0).getDate();
+				var monthCountThis = new Date(response.Y, response.m - 0, 0).getDate();
+
+				response.cal = {};
+				response.cal.table = [];
+				response.cal.first = firstDayOfWeek - 1;
+				response.cal.current = response.cal.first - 1 + response.d;
+				response.cal.last = response.cal.first - 1 + monthCountThis;
+
+				for (var i = 0; i < response.cal.first; i++) {
+					response.cal.table.push(monthCountLast - i);
+				}
+				for (var i = 0; i < monthCountThis; i++) {
+					response.cal.table.push(i + 1);
+				}
+				for (var i = 0; i < 7 * 6 - (response.cal.last + 1); i++) {
+					response.cal.table.push(i + 1);
+				}
+			}
 
 			this.response = response;
 		},
