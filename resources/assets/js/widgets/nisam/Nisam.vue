cba922e2ba3d8de61db2e96452881f0b8771f4aa
Franjo Filo <fffilo666@gmail.com>
2016-11-30 15:45:33 +0100

    Widget nisam css

--- resources/assets/js/widgets/nisam/Nisam.vue
+++ resources/assets/js/widgets/nisam/Nisam.vue
@@ -16,7 +16,34 @@ export default {
                 error: null
             }
         };
+    },
+
+    methods: {
+        getVotes() {
+            let result = [];
+
+            this.response.data.votes.forEach((value) => {
+                if (value.votes) {
+                    result.push(value);
+                }
+            });
+
+            result.sort((a,b) => {
+                if (a.votes < b.votes) return +1;
+                if (a.votes > b.votes) return -1;
+                //if (a.name  < b.name)  return +1;
+                //if (a.name  > b.name)  return -1;
+                if (a.name.localeCompare(b.name)) return a.name.localeCompare(b.name);
+                if (a.id    < b.id)    return +1;
+                if (a.id    > b.id)    return -1;
+
+                return 0;
+            });
+
+            return result;
+        }
     }
+
 }
 
 </script>
