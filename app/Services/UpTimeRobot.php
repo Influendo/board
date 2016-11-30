45ce09871295a25658f701c5886945bcfab8413c
Boris Strahija <bstrahija@gmail.com>
2016-11-30 13:45:40 +0100

    Fix

--- app/Services/UpTimeRobot.php
+++ app/Services/UpTimeRobot.php
@@ -16,22 +16,14 @@ class UpTimeRobot
         ]);
     }
 
+    /**
+     * Fetch monitors
+     *
+     * @param  array $ids
+     * @return Response
+     */
     public function monitors($ids = [])
     {
-        return @json_decode(''
-. '{"monitors":'
-. '{"monitor":['
-. '{"id":"778089655","friendlyname":"OptimizeLeads DEV","url":"http://opleads.optimizegraphics.com","type":"1","subtype":"","keywordtype":"","keywordvalue":"","httpusername":"","httppassword":"","port":"","interval":"300","status":"2","alltimeuptimeratio":"99.81"}'
-. ','
-. '{"id":"778089656","friendlyname":"OptimizeLeads","url":"https://www.optimizeleads.com","type":"1","subtype":"","keywordtype":"","keywordvalue":"","httpusername":"","httppassword":"","port":"","interval":"300","status":"2","alltimeuptimeratio":"100"}'
-. ','
-. '{"id":"778340258","friendlyname":"Nisam","url":"http://nisam.goxlab.com","type":"1","subtype":"","keywordtype":"","keywordvalue":"","httpusername":"","httppassword":"","port":"","interval":"300","status":"2","alltimeuptimeratio":"99.9"}'
-. ','
-. '{"id":"778349480","friendlyname":"OptimizePress","url":"http://www.optimizepress.com/","type":"1","subtype":"","keywordtype":"","keywordvalue":"","httpusername":"","httppassword":"","port":"","interval":"300","status":"2","alltimeuptimeratio":"100"}'
-. ']}'
-. '}'
-);
-
         $ids = implode("-", $ids);
         $response = $this->client->request('GET', 'getMonitors', [
             'query' => ['apiKey' => env('UPTIMEROBOT_KEY'), 'format' => 'json', 'nojsoncallback' => 1, 'monitors' => $ids]
