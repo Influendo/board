7325fc6ca9e09b4c98e3d43342411d0c927de4b3
Boris Strahija <bstrahija@gmail.com>
2016-12-14 09:51:41 +0100

    Docs and timeout

--- app/Services/Nisam.php
+++ app/Services/Nisam.php
@@ -4,18 +4,29 @@ namespace App\Services;
 
 use GuzzleHttp\Client;
 
+/**
+ * Handle calls to Nisam API
+ */
 class Nisam
 {
+    /**
+     * Init service
+     */
     public function __construct()
     {
         $this->client = new Client([
             // Base URI is used with relative requests
             'base_uri' => env('NISAM_API_URL', 'http://nisam.goxlab.com'),
             // You can set any number of default request options.
-            'timeout'  => 2.0,
+            'timeout'  => 10.0,
         ]);
     }
 
+    /**
+     * Get latest status from API
+     *
+     * @return array
+     */
     public function status()
     {
         $response = $this->client->request('GET', 'api/status');
