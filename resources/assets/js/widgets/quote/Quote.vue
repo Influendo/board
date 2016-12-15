f3796fbe76e259393a2355524c9b52b0efeea50e
Franjo Filo <fffilo666@gmail.com>
2016-12-15 11:17:46 +0100

    Widget quote calc font-size

--- resources/assets/js/widgets/quote/Quote.vue
+++ resources/assets/js/widgets/quote/Quote.vue
@@ -14,7 +14,30 @@ export default {
         };
     },
 
+    mounted() {
+        $(window).on('resize', this.setFontSize);
+    },
+
+    updated() {
+        this.setFontSize();
+    },
+
     methods: {
+        setFontSize() {
+            let $parent  = $(this.$el);
+            let $success = $parent.find('.modal.success');
+            let $error   = $parent.find('.modal.error');
+
+            // set default
+            $parent.css('font-size', '');
+
+            // decrease font-size while element scroll-height is larger than it's actual height
+            while (parseInt($parent.css('font-size')) > 16 && ($success.height() > $parent.height() || $error.height() > $parent.height())) {
+                console.log(parseInt($parent.css('font-size')), $parent.prop('scrollHeight'), $parent.height());
+                $parent.css('font-size', '-=0.1vh');
+            }
+        },
+
         background(value) {
             if (this.response.data) {
                 return 'background-image:url(' + this.response.data.contents.quotes[0].background + ')';
