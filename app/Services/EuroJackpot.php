a5ee202af1ac97a7067f15d89b01ee84b760fa42
Zvonko Biskup <coder@codeforest.net>
2018-10-05 14:32:16 +0200

    Scraping Eurojackpot - thanks Franjo

--- app/Services/EuroJackpot.php
+++ app/Services/EuroJackpot.php
@@ -19,76 +19,83 @@ class EuroJackpot
 
     public function index($cache = null)
     {
+
         // since we only have 10 requests per months
         // allowed on api we'll cache result and
         // store it until next week
         $cache = Cache::rememberForever('eurojackpot', function() {
-            // @todo - put this in .env?
-            $api_key = 'FAPkC8n32PS9Qz3hPM';
-            //$api_key = 'e2tv3Rc2eUMudy8Q4T';
-            //$api_key = 'bRWe9DPgStHY4VFagG';
-            $game = 'eurojackpot';
-            $draw = '';
+            $url = 'https://www.lutrija.hr/cms/eurojackpot';
+            $html = file_get_contents($url);
+            $lastRound = '0';
+            $lastDate = '00.00.0000';
+            $lastDraw = array();
+            $nextRound = '0';
+            $nextDate = '00.00.0000';
+            $nextAmount = "0,00 kn";
 
-            // @todo - refactore
-            $url = $this->jackpot;
-            $url = str_replace('{api_key}', $api_key, $url);
-            $url = str_replace('{game}', $game, $url);
-            $url = str_replace('{draw}', $draw, $url);
-            //$response = $this->client->request('GET', $url);
-            //$jackpot = @json_decode((string) $response->getBody());
-            $jackpot = @json_decode('{"currency":"HRK","error":0,"jackpot":"113000000","next_draw":"2018-07-13"}');
-            // api is returning wrong next draw
-            //$draw = date('Y-m-d', strtotime('-7 days', strtotime($jackpot->next_draw)));
+            $subject = $html;
+            $pattern = '/<p id="date-info">[\s\S]*?<span>([\s\S]*?) odigrano ([\s\S]*?)<\/span>/';
+            if (preg_match($pattern, $subject, $matches)) {
+                $lastRound = trim($matches[1]);
+                $lastDate = trim($matches[2]);
+            }
 
-            $date = new Carbon('last friday');
-            $draw = $date->format('Y-m-d');
+            $subject = $html;
+            $pattern = '/<div id="winnings-info">[\s\S]*?<ul.*?>([\s\S]*?)<\/ul>/';
+            if (preg_match($pattern, $subject, $matches)) {
+                $subject = $matches[1];
+                $pattern = '/<li.*?>(.*?)<\/li>/';
+                if (preg_match_all($pattern, $subject, $matches)) {
+                    $lastDraw = $matches[1];
+                }
+            }
 
-            // as we get the wrong next draw date, we are making it manually
-            $next = new Carbon('this friday');
-            $next_draw = $next->format('Y-m-d');
+            $subject = $html;
+            $pattern = '/(\d+\. kolo) – izvlačenje brojeva u .*? (\d+\.\d+\.\d+\.)/';
+            if (preg_match($pattern, $subject, $matches)) {
+                $nextRound = $matches[1];
+                $nextDate = $matches[2];
+            }
 
-            // @todo - refactore
-            $url = $this->results;
-            $url = str_replace('{api_key}', $api_key, $url);
-            $url = str_replace('{game}', $game, $url);
-            $url = str_replace('{draw}', $draw, $url);
-            $response = $this->client->request('GET', $url);
-            $results = @json_decode((string) $response->getBody());
-            //\Log::info('results ' . print_r($response->getBody(), true));
-            //$results = @json_decode('{"error":0,"draw":"2018-07-06","results":"02,07,24,38,45,05,08"}');
+            $subject = $html;
+            $pattern = '/<div class="jackpot-info">[\s\S]*?<h4.*?>(.*?)<\/h4>/';
+            $pattern = '/<h3 class="countdown-prize".*?>([\s\S]*?)<\/h3>/';
+            if (preg_match($pattern, $subject, $matches)) {
+                $nextAmount = trim($matches[1]);
+                $nextAmount = trim($nextAmount, chr(194) . chr(160));
+            }
 
             return json_encode([
                 'nextDraw' => [
-                    'id' => date('W', strtotime($next_draw)),
-                    'datetime' => $next_draw  . ' 21:00:00',
+                    'id' => $nextRound,
+                    'datetime' => $nextDate,
                     'tv' => null,
-                    'jackpot' => number_format($jackpot->jackpot, 0, ',', '.') . ' ' . $jackpot->currency,
+                    'jackpot' => $nextAmount,
                     'prizes' => null,
                     'paymentLock' => null,
                     'mainTable' => null,
                     'starTable' => null,
                 ],
                 'lastDraw' => [
-                    'id' => date('W', strtotime($draw)),
-                    'datetime' => $draw . ' 21:00:00',
+                    'id' => $lastRound,
+                    'datetime' => $lastDate,
                     'tv' => null,
                     'jackpot' => null,
                     'prizes' => [],
                     'paymentLock' => null,
-                    'mainTable' => array_slice(explode(',', $results->results), 0, 5),
-                    'starTable' => array_slice(explode(',', $results->results), -2, 2),
+                    'mainTable' => array_slice($lastDraw, 0, 5),
+                    'starTable' => array_slice($lastDraw, -2, 2),
                 ],
             ]);
         });
 
-        // recache 3 days after draw (monday
+        // recache 2 days after draw (monday
         // is the best day to do that ;-))
         $result = json_decode($cache);
         //\Log::info(print_r($cache, true));
         $now = Carbon::now('Europe/Zagreb')->timestamp;
         $draw = Carbon::parse($result->nextDraw->datetime,'Europe/Helsinki')->timestamp;
-        if ($now - $draw > 60*60*24*3) {
+        if ($now - $draw > 60*60*24*2) {
             Cache::forget('eurojackpot');
         }
 
